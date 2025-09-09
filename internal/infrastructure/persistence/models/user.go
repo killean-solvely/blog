52156b1141b0420634696c2e47e434c1f9e54d5f
Killean Johnson <killean@Killeans-MacBook-Pro.local>
2025-09-08 22:01:37 -0400

    Fix bug where users weren't properly marshalling in sqlite get

--- internal/infrastructure/persistence/models/user.go
+++ internal/infrastructure/persistence/models/user.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type User struct {
-	ID           string    `json:"id"`
-	Email        string    `json:"email"`
-	PasswordHash string    `json:"password_hash"`
-	Username     string    `json:"username"`
-	Description  string    `json:"description"`
-	UserRoles    string    `json:"user_roles"`
-	JoinDate     time.Time `json:"join_date"`
+	ID           string    `db:"id"`
+	Email        string    `db:"email"`
+	PasswordHash string    `db:"password_hash"`
+	Username     string    `db:"username"`
+	Description  string    `db:"description"`
+	UserRoles    string    `db:"user_roles"`
+	JoinDate     time.Time `db:"join_date"`
 }
